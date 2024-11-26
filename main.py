240834a1da3a4a33ef535cbaa0a08d038edc6e14
JT-Tsai <41375007h@gapps.ntnu.edu.tw>
2024-11-26 10:41:53 +0800

    test_main

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from base import Agent
 from execution_pipeline import main
+from colorama import Fore, Style
 from transformers import (
     AutoTokenizer,
     AutoModelForCausalLM,
     set_seed,
     BitsAndBytesConfig,
 )
-from utils import RAG
+from utils import RAG, strip_all_lines
 import torch
+import re
+import random
+
 
 import ipdb
 
 
+
 def get_bnb_config():
-    """function for model quantization with bf16 setting"""
+    """function for model quantization with int8 setting"""
     quantization_config = BitsAndBytesConfig(
-        load_in_4bit = True,
-        bnb_4bit_compute_dtype = torch.bfloat16,
-        bnb_4bit_use_double_quant = True,
-        bnb_4bit_quant_type = "n4f",
+        load_in_8bit = True,
+        llm_int8_has_fp16_weight=False
+        # llm_int8_has_fp16_weight=True
     )
 
     return quantization_config
@@ -32,6 +36,9 @@ class ClassificationAgent(Agent):
     def __init__(self, config: dict) -> None:
         """setting agent elements like `device`, `tokenizer`, `model`, `quantization_config`, 
             `rag_config(utils)`, `prompt_format`, `buffer`"""
+        
+        super().__init__(config)
+        
         # device
         self.device = config.get("device")
         
@@ -49,17 +56,25 @@ class ClassificationAgent(Agent):
 
 
         print(f"load model which name is {model_name}")
-        if quantization_type == "bf16":
-            self.model = AutoModelForCausalLM.from_pretrained(model_name, quantization_type = get_bnb_config(), torch_dtype = torch.bfloat16)
+        if quantization_type == "use_8bit":
+            self.model = AutoModelForCausalLM.from_pretrained(
+                model_name, 
+                quantization_type = get_bnb_config(), 
+                device_map = self.device
+            )
             print(f"load model using quantization")
-        elif quantization_type == "fp16":
-            # self.model = None
-            pass
         else:
-            self.model = AutoModelForCausalLM.from_pretrained(model_name)
+            weight_type = config.get("weight_type")
+            self.model = AutoModelForCausalLM.from_pretrained(
+                model_name,
+                torch_dtype = torch.bfloat16 if weight_type == "bf16" else torch.float16,
+                device_map = self.device,
+            )
+
+        self.model.eval()
 
         # rag_config
-        self.rag_config = {
+        rag_config = {
             # I think we need to finetune embedding_model when we select the model to implement.
             "embedding_model": config.get("embedding_model") \
                 if config.get("embedding_model") is not None else "BAAI/bge-base-en-v1.5",
@@ -67,7 +82,96 @@ class ClassificationAgent(Agent):
             "top_k": config.get("top_k") if config.get("top_k") is not None else 5,
             "order": config.get("order") if config.get("order") is not None else "similar_at_top",
         }
+        self.rag = RAG(rag_config)
 
+        # gen max token
+        self.max_token = config.get("max_token") if config.get("max_token") is not None else 32
+
+        # save the streaming inputs and outputs for iterative improvement
+        self.inputs = list()
+        self.model_outputs = list()
+
+    def generate_response(self, message: list) -> str:
+        text_chat = self.tokenizer.apply_chat_template(
+            message,
+            tokenize=False, # no covert to embedding in this step
+            add_generation_prompt=True
+        )
+        model_inputs = self.tokenizer([text_chat], return_tensors="pt").to(self.device)
+        
+        # optimize VRAM
+        with torch.inference_mode():
+            generated_ids = self.model.generate(
+                **model_inputs,
+                max_new_tokens = self.max_token,
+                do_sample = False
+            )
+
+        # ignore the input partial, keep the output partial to the result
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+        
+        return self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    def get_prompt(text, option_text, shots = None):
+        if shots is None:
+            prompt = f""" \
+            Act as a professional medical doctor that can diagnose the patient based on the patient profile.
+            Provide your diagnosis in the following format: ID: <number>, <diagnosis>
+
+            {text}
+
+            All possible diagnoses for you to choose from are as follows (one diagnosis per line, in the format of ID: <number>, <diagnosis>):
+            {option_text}
+
+            Now, directly provide the diagnosis for the patient use the above format. Don't give me any additional information.""".strip()
+        else:
+            prompt = f"""\
+            Act as a professional medical doctor that can diagnose the patient based on the patient profile.
+            Provide your diagnosis in the following format: ID: <number>, <diagnosis>
+            
+            All possible diagnoses for you to choose from are as follows (one diagnosis per line, in the format of ID: <number>, <diagnosis>):
+            {option_text}
+
+            The following are some cases for reference.
+            
+            {shots}
+            
+            Now it's your turn.
+            
+            {text}
+            
+            Now, directly provide the diagnosis for the patient use the above format. Don't give me any additional information.""".strip()
+
+        return strip_all_lines(prompt)
+    
+    def get_shot_template(question, answer) -> str:
+        prompt = f"""profile content: {question}, Answer: {answer}"""
+        return strip_all_lines(prompt)
+    
+    def extract_label(pred_text: str, label2desc: dict[str, str]) -> str:
+        prediction = re.findall(pattern = r"ID: (\d+)", string = pred_text)
+        prediction = [k.replace("ID: ", "") for k in prediction]
+        
+        result = None
+
+        if len(prediction) == 1:
+            id = prediction[0]
+            if int(id) in label2desc:
+                result = id
+            else:
+                print(Fore.RED + f"Prediction {pred_text} not found in the label set. Randomly select one." + Style.RESET_ALL)
+                result = random.choice(list(label2desc.keys()))
+        else:
+            if len(result) > 1:
+                print(Fore.YELLOW + f"Extracted numbers {prediction} is not exactly one. Select the first one." + Style.RESET_ALL)
+                result = id[0]
+            else:
+                print(Fore.RED + f"Prediction {pred_text} has no extracted numbers. Randomly select one." + Style.RESET_ALL)
+                result = random.choice(list(label2desc.keys()))
+
+        return str(result)
 
 
     def __call__(
@@ -75,41 +179,41 @@ class ClassificationAgent(Agent):
         label2desc: dict[str, str],
         text: str
     ) -> str:
+        option_text = '\n'.join([f"ID: {str(k)}, {v}" for k, v in label2desc.items()])
         ipdb.set_trace()
-        """
-        Classify the text into one of the labels.
+        shots = self.rag.retrieve(query = text, top_k = self.rag.top_k) if (self.rag.insert_acc > 0) else []
+        if len(shots):
+            prompt = self.get_prompt(text, option_text, shots)
+        else:
+            prompt = self.get_prompt(text, option_text)
 
-        Args:
-            label2desc (dict[str, str]): A dictionary mapping each label to its description.
-            text (str): The text to classify.
+        messages = [
+            {"role": "user", "content": prompt}
+        ]
+        response = self.generate_response(messages)
+        prediction = self.extract_label(response, label2desc)
 
-        Returns:
-            str: The label (should be a key in label2desc) that the text is classified into.
+        self.update_log_info(log_data={
+            "num_input_tokens": len(self.tokenizer.encode(prompt)),
+            "num_output_tokens": len(self.tokenizer.encode(response)),
+            "num_shots": str(len(shots)),
+            "input_pred": prompt,
+            "output_pred": response,
+        })
 
-        For example:
-        label2desc = {
-            "apple": "A fruit that is typically red, green, or yellow.",
-            "banana": "A long curved fruit that grows in clusters and has soft pulpy flesh and yellow skin when ripe.",
-            "cherry": "A small, round stone fruit that is typically bright or dark red.",
-        }
-        text = "The fruit is red and about the size of a tennis ball."
-        label = "apple" (should be a key in label2desc, i.e., ["apple", "banana", "cherry"])
-        """
-        # TODO
-        raise NotImplementedError
+        self.inputs.append(text)
+        self.model_outputs.append(f"ID: {str(prediction)}, {label2desc[int(prediction)]}")
+        return prediction
 
     def update(self, correctness: bool) -> bool:
-        """
-        Update your LLM agent based on the correctness of its own prediction at the current time step.
-
-        Args:
-            correctness (bool): Whether the prediction is correct.
-
-        Returns:
-            bool: Whether the prediction is correct.
-        """
-        # TODO
-        raise NotImplementedError
+        if correctness:
+            question = self.inputs[-1]
+            answer = self.model_outputs[-1]
+            chunk = self.get_shot_template(question, answer)
+            self.rag.insert(key = question, value = chunk)
+            return True
+        else:
+            return False
 
 class SQLGenerationAgent(Agent):
     """
